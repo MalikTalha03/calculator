f7f50f9a405ba28b7a87d9b2a3bcf2a9d2deb6b7
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-22 01:00:08 +0500

    Refactor Inputfield component

--- src/components/Inputfield.js
+++ src/components/Inputfield.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {View, StyleSheet, TextInput} from 'react-native';
 import styles from './styles';
 
-const Inputfield = (props) => {
+const Inputfield = props => {
   const {input} = styles;
   return (
     <View>
-      <TextInput style={input} keyboardType="none" value={props.input} />
+      <TextInput style={input} value={props.input} />
     </View>
   );
 };
