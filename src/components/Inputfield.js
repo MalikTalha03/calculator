e56d64bd81720a5dd361517d20df4866ed8951dd
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-01-22 00:52:09 +0500

    Add Inputfield component

---
+++ src/components/Inputfield.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet, TextInput} from 'react-native';
+import styles from './styles';
+
+const Inputfield = (props) => {
+  const {input} = styles;
+  return (
+    <View>
+      <TextInput style={input} keyboardType="none" value={props.input} />
+    </View>
+  );
+};
+
+export default Inputfield;
